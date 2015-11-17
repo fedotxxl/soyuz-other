d6c28e9b520130c67e3b040d6006c07bda59455e
fbelov <fedor.belov@mail.ru>
2015-11-17 23:06:38 +0300

    locker map

--- soyuz-concurrent/src/main/java/io/belov/soyuz/concurrent/LockerMap.java
+++ soyuz-concurrent/src/main/java/io/belov/soyuz/concurrent/LockerMap.java
@@ -1,9 +1,14 @@
 package io.belov.soyuz.concurrent;
 
+import com.google.common.base.Function;
+import com.google.common.base.Throwables;
+
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.Callable;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
+import java.util.function.Consumer;
 
 /**
  * Created by fbelov on 17.11.15.
@@ -23,4 +28,59 @@ public class LockerMap<T> {
         return answer;
     }
 
+    public void lockAndDo(T key, Runnable action) {
+        Lock lock = get(key);
+
+        try {
+            lock.lock();
+
+            action.run();
+        } catch (Exception e) {
+            throw Throwables.propagate(e);
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public void lockAndDo(T key, Consumer<T> action) {
+        Lock lock = get(key);
+
+        try {
+            lock.lock();
+
+            action.accept(key);
+        } catch (Exception e) {
+            throw Throwables.propagate(e);
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public <V> V lockAndDo(T key, Callable<V> action) {
+        Lock lock = get(key);
+
+        try {
+            lock.lock();
+
+            return action.call();
+        } catch (Exception e) {
+            throw Throwables.propagate(e);
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public <V> V lockAndDo(T key, Function<T, V> action) {
+        Lock lock = get(key);
+
+        try {
+            lock.lock();
+
+            return action.apply(key);
+        } catch (Exception e) {
+            throw Throwables.propagate(e);
+        } finally {
+            lock.unlock();
+        }
+    }
 }
