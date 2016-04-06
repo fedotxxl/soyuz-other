3625303b7efc4324a100ccb80455ca6d2d207e3c
fbelov <fedor.belov@mail.ru>
2016-04-06 19:49:15 +0300

    prometheus

---
+++ soyuz-metrics-prometheus/src/main/java/io/belov/soyuz/metrics/prometheus/PrometheusGatewayBootstrap.java
@@ -0,0 +1,47 @@
+package io.belov.soyuz.metrics.prometheus;
+
+import io.belov.soyuz.log.LoggerEvents;
+import io.belov.soyuz.utils.is;
+import io.belov.soyuz.utils.to;
+import io.prometheus.client.CollectorRegistry;
+import io.prometheus.client.exporter.PushGateway;
+
+import java.io.IOException;
+
+/**
+ * Created by fbelov on 06.04.16.
+ */
+public class PrometheusGatewayBootstrap {
+
+    private static final LoggerEvents loge = LoggerEvents.getInstance(PrometheusGatewayBootstrap.class);
+
+    public PrometheusGatewayBootstrap(String gatewayUrl, String job) {
+        if (is.tt(gatewayUrl) && is.tt(job)) {
+            pushMetricsTo(gatewayUrl, job);
+        }
+    }
+
+    private void pushMetricsTo(String gatewayUrl, String job) {
+        loge.debug("metrics.prometheus.configurePushGateway", to.map("url", gatewayUrl));
+
+        Thread thread = new Thread(() -> {
+            PushGateway pg = new PushGateway(gatewayUrl);
+
+            try {
+                while (true) {
+                    try {
+                        pg.pushAdd(CollectorRegistry.defaultRegistry, job);
+                    } catch (IOException e) {
+                        loge.error("metrics.promtheus.e", to.map("e", e.toString()));
+                    }
+
+                    Thread.sleep(5000);
+                }
+            } catch (InterruptedException e) {
+            }
+        });
+
+        thread.setDaemon(true);
+        thread.start();
+    }
+}
