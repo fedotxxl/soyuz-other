30f0987b4750292d5aad48dbc09a9b4b75dcefd4
fbelov <fbelov@gramant.ru>
2016-12-03 13:53:01 +0300

    utils-json

--- soyuz-json/src/main/java/io/belov/soyuz/json/JacksonUtils.java
+++ soyuz-json/src/main/java/io/belov/soyuz/json/JacksonUtils.java
@@ -1,5 +1,6 @@
 package io.belov.soyuz.json;
 
+import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectReader;
@@ -14,16 +15,23 @@ import org.slf4j.LoggerFactory;
 public class JacksonUtils {
 
     private static final Logger log = LoggerFactory.getLogger(JacksonUtils.class);
-
     private static final ObjectWriter writer = new ObjectMapper().writer();
     private static final ObjectReader reader = new ObjectMapper().reader();
+    private static final ObjectMapper jsReader;
+
+    static {
+        //http://stackoverflow.com/questions/26591359/parsing-non-json-javascript-object-with-jackson
+        jsReader = new ObjectMapper();
+        jsReader
+                .configure(JsonParser.Feature.ALLOW_UNQUOTED_FIELD_NAMES, true)
+                .configure(JsonParser.Feature.ALLOW_SINGLE_QUOTES, true)
+                .configure(JsonParser.Feature.ALLOW_COMMENTS, true);
+    }
 
     public static String toJson(Object o) {
         try {
             return writer.writeValueAsString(o);
         } catch (JsonProcessingException e) {
-            log.warn("Exception on parsing object {} to JSON", o);
-
             throw Throwables.propagate(e);
         }
     }
@@ -35,10 +43,18 @@ public class JacksonUtils {
     public static <T> T fromJson(String json, Class<T> clazz) {
         try {
             return reader.withType(clazz).readValue(json);
-        } catch (Throwable t) {
+        } catch (Throwable e) {
             log.warn("Exception on parsing JSON {} to {}", json, clazz);
+            throw Throwables.propagate(e);
+        }
+    }
 
-            throw Throwables.propagate(t);
+    public static <T> T fromJavaScript(String js, Class<T> clazz) {
+        try {
+            return jsReader.readValue(js, clazz);
+        } catch (Throwable e) {
+            log.warn("Exception on parsing JavaScript {} to {}", js, clazz);
+            throw Throwables.propagate(e);
         }
     }
 
