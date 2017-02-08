086cb6158a647b9bcb72bd92aa1dde27765855aa
fbelov <fbelov@gramant.ru>
2017-02-08 13:00:15 +0300

    prometheus

--- soyuz-metrics-prometheus/src/main/java/io/belov/soyuz/metrics/prometheus/Prometheus.java
+++ soyuz-metrics-prometheus/src/main/java/io/belov/soyuz/metrics/prometheus/Prometheus.java
@@ -1,5 +1,6 @@
 package io.belov.soyuz.metrics.prometheus;
 
+import io.belov.soyuz.utils.is;
 import io.belov.soyuz.utils.to;
 import io.prometheus.client.Histogram;
 
@@ -17,11 +18,15 @@ public class Prometheus {
     private static final Map<String, Histogram> HISTOGRAMS_BY_NAME = new HashMap<>();
 
     public static void doAndTime(String metricName, Runnable action) {
-        doAndTime(getMetricByName(metricName), null, action);
+        doAndTime(getMetricByName(metricName), (String) null, action);
     }
 
     public static void doAndTime(Histogram metric, Runnable action) {
-        doAndTime(metric, null, action);
+        doAndTime(metric, (String) null, action);
+    }
+
+    public static void doAndTime(Histogram metric, @Nullable String label, Runnable action) {
+        doAndTime(metric, (is.t(label)) ? to.list(label) : null, action);
     }
 
     public static void doAndTime(Histogram metric, @Nullable List<String> labels, Runnable action) {
@@ -43,7 +48,11 @@ public class Prometheus {
     }
 
     public static <T> T doAndTime(Histogram metric, Callable<T> action) {
-        return doAndTime(metric, null, action);
+        return doAndTime(metric, (String) null, action);
+    }
+
+    public static <T> T doAndTime(Histogram metric, @Nullable String label, Callable<T> action) {
+        return doAndTime(metric, (is.t(label)) ? to.list(label) : null, action);
     }
 
     public static <T> T doAndTime(Histogram metric, @Nullable List<String> labels, Callable<T> action) {
