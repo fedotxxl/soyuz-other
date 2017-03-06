d7dac9cf74ad4b223f1175130491218cfc9a203d
fbelov <fbelov@gramant.ru>
2017-03-06 14:03:12 +0300

    metrics-prometheus

--- soyuz-metrics-prometheus/src/main/java/io/belov/soyuz/metrics/prometheus/MetricsPrometheusService.java
+++ soyuz-metrics-prometheus/src/main/java/io/belov/soyuz/metrics/prometheus/MetricsPrometheusService.java
@@ -1,11 +1,10 @@
 package io.belov.soyuz.metrics.prometheus;
 
-import io.belov.soyuz.utils.thread;
+import io.belov.soyuz.log.LoggerEvents;
+import io.belov.soyuz.utils.to;
 import io.prometheus.client.CollectorRegistry;
 import io.prometheus.client.Histogram;
 import io.prometheus.client.exporter.PushGateway;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.util.concurrent.Callable;
@@ -15,24 +14,20 @@ import java.util.concurrent.Callable;
  */
 public class MetricsPrometheusService {
 
-    private static final Logger log = LoggerFactory.getLogger(MetricsPrometheusService.class);
+    private static final LoggerEvents loge = LoggerEvents.getInstance(MetricsPrometheusService.class);
 
     public void pushMetricsEveryMillis(String url, long intervalInMillis) {
         if (url == null || intervalInMillis <= 0 || url.length() == 0) return;
 
-        thread.startDaemon(() -> {
-            PushGateway pg = new PushGateway(url);
+        PushGateway pg = new PushGateway(url);
 
-            while (true) {
-                try {
-                    pg.pushAdd(CollectorRegistry.defaultRegistry, "app");
-                } catch (IOException e) {
-                    log.error("Exception on sending metrics to gateway - " + e.toString());
-                }
-
-                thread.sleep(intervalInMillis);
+        to.daemonForever("prometheus.push", intervalInMillis, () -> {
+            try {
+                pg.pushAdd(CollectorRegistry.defaultRegistry, "app");
+            } catch (IOException e) {
+                loge.error("prometheus.push.e", to.map("e", e.toString()));
             }
-        });
+        }).start();
     }
 
     public void doAndTime(Histogram metric, Runnable action) {
