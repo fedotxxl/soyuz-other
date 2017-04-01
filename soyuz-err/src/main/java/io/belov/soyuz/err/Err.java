1c4e3dace054617597312c73cb5ffc8962d2c64d
fbelov <filya@devadmin.com>
2017-04-02 00:08:33 +0300

    err builder

--- soyuz-err/src/main/java/io/belov/soyuz/err/Err.java
+++ soyuz-err/src/main/java/io/belov/soyuz/err/Err.java
@@ -1,6 +1,7 @@
 package io.belov.soyuz.err;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.Builder;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.ToString;
@@ -38,36 +39,45 @@ public class Err<V> {
     private Err() {
     }
 
-    private Err(String field, String code, String message, V value, Object[] args) {
+    @Builder
+    private Err(String field, String code, Code codeAsObject, String message, V value, Object[] args) {
         this.field = field;
-        this.code = code;
+        this.code = (codeAsObject != null) ? codeAsObject.getErrCode() : code;
         this.message = message;
         this.value = value;
         this.args = args;
     }
+//
+//    public static Err reject(Code code, Object... args) {
+//        return reject(code.getErrCode(), args);
+//    }
+//
+//    public static Err reject(String code, Object... args) {
+//        return new Err<>(null, code, null, null, args);
+//    }
+//
+//    public static <V> Err<V> reject(String field, Code code, V value) {
+//        return reject(field, code.getErrCode(), value);
+//    }
+//
+//    public static <V> Err<V> reject(String field, String code, V value) {
+//        return new Err<>(field, code, null, value, null);
+//    }
+//
+//    public static <V> Err<V> reject(String field, Code code, V value, Object... args) {
+//        return reject(field, code.getErrCode(), value, args);
+//    }
+//
+//    public static <V> Err<V> reject(String field, String code, V value, Object... args) {
+//        return new Err<>(field, code, null, value, args);
+//    }
 
-    public static Err reject(Code code, Object... args) {
-        return reject(code.getErrCode(), args);
+    public static ErrBuilder code(String code) {
+        return Err.builder().code(code);
     }
 
-    public static Err reject(String code, Object... args) {
-        return new Err<>(null, code, null, null, args);
-    }
-
-    public static <V> Err<V> reject(String field, Code code, V value) {
-        return reject(field, code.getErrCode(), value);
-    }
-
-    public static <V> Err<V> reject(String field, String code, V value) {
-        return new Err<>(field, code, null, value, null);
-    }
-
-    public static <V> Err<V> reject(String field, Code code, V value, Object... args) {
-        return reject(field, code.getErrCode(), value, args);
-    }
-
-    public static <V> Err<V> reject(String field, String code, V value, Object... args) {
-        return new Err<>(field, code, null, value, args);
+    public static ErrBuilder field(String field) {
+        return Err.builder().field(field);
     }
 
     public interface Code {
