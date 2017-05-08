78f85f308baa38544418806d9fe5df8b88eb60ee
fbelov <fbelov@gramant.ru>
2017-05-08 15:35:00 +0300

    jacksonMapper

---
+++ soyuz-json/src/main/java/io/belov/soyuz/json/JacksonMapper.java
@@ -0,0 +1,43 @@
+package io.belov.soyuz.json;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import javax.annotation.Nullable;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * Created on 16.01.17.
+ */
+public class JacksonMapper {
+
+    private JacksonService jacksonService;
+
+    public JacksonMapper() {
+        this.jacksonService = new JacksonService(new ObjectMapper());
+    }
+
+    public String toJson(Object object) {
+        return jacksonService.toJson(object);
+    }
+
+    public String toJsonOrNull(Object object) {
+        return toJsonOrNull(object);
+    }
+
+    @Nullable
+    public <T> T fromJson(@Nullable String json, Class<T> clazz) {
+        return jacksonService.fromJson(json, clazz);
+    }
+
+    @Nullable
+    public <T> List<T> fromJsonList(@Nullable String json, Class<T> clazz) {
+        return jacksonService.fromJsonList(json, clazz);
+    }
+
+    @Nullable
+    public <T> Set<T> fromJsonSet(@Nullable String json, Class<T> clazz) {
+        return jacksonService.fromJsonSet(json, clazz);
+    }
+
+}
