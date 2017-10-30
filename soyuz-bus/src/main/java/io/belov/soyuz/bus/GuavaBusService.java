aefdced3d5134ce6ad95b56e6cfff05e23fd183e
fbelov <fedor.belov@mail.ru>
2017-10-30 22:30:47 +0300

    guava bus bug fix

--- soyuz-bus/src/main/java/io/belov/soyuz/bus/GuavaBusService.java
+++ soyuz-bus/src/main/java/io/belov/soyuz/bus/GuavaBusService.java
@@ -4,18 +4,20 @@ import com.google.common.collect.ImmutableMap;
 import com.google.common.eventbus.EventBus;
 import io.thedocs.soyuz.log.LoggerEvents;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextRefreshedEvent;
 
-import javax.annotation.PostConstruct;
 import java.util.Collection;
 import java.util.stream.Collectors;
 
 /**
  * Created by fbelov on 16.09.15.
  */
-public class GuavaBusService {
+public class GuavaBusService implements ApplicationListener<ContextRefreshedEvent> {
 
     private static final LoggerEvents loge = LoggerEvents.getInstance(GuavaBusService.class);
 
+    private boolean isBeansRegistered = false;
     private ApplicationContext applicationContext;
     private EventBus bus;
 
@@ -24,18 +26,18 @@ public class GuavaBusService {
         this.bus = bus;
     }
 
-    @PostConstruct
-    private void setUp() {
-        registerBusBeans(applicationContext);
-    }
+    @Override
+    public void onApplicationEvent(ContextRefreshedEvent event) {
+        if (!isBeansRegistered) {
+            Collection<GuavaBusBean> beans = applicationContext.getBeansOfType(GuavaBusBean.class).values();
 
-    public void registerBusBeans(ApplicationContext applicationContext) {
-        Collection<GuavaBusBean> beans = applicationContext.getBeansOfType(GuavaBusBean.class).values();
+            loge.debug("bus.registerListeners", ImmutableMap.of("listeners", beans.stream().map(b -> b.getClass().getName()).collect(Collectors.toList())));
 
-        loge.debug("bus.registerListeners", ImmutableMap.of("listeners", beans.stream().map(b -> b.getClass().getName()).collect(Collectors.toList())));
+            for (GuavaBusBean bean : beans) {
+                bus.register(bean);
+            }
 
-        for (GuavaBusBean bean : beans) {
-            bus.register(bean);
+            isBeansRegistered = true;
         }
     }
 }
